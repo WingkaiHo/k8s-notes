8fda9529fc7029c07eaa71abe31bc003d8d2f441
yongjiahe <yongjiahe@tuputech.com>
2018-08-30 20:06:55 +0800

    add readme for rabc builder

--- gitlab-ci/namespace-rabc-builder/set_kubectl_context-tmpl.sh
+++ gitlab-ci/namespace-rabc-builder/set_kubectl_context-tmpl.sh
@@ -2,9 +2,8 @@
 
 KUBE_URL=https://__KUBE_ClUSTER_URL__
 KUBE_NAMESPACE=__YOUR_GITLAB_BUILD_NAMESPACE__
-
-
-kubectl config set-cluster default-cluster --server=${KUBE_URL} --certificate-authority=./ca.crt
+CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+kubectl config set-cluster default-cluster --server=${KUBE_URL} --certificate-authority=${CURRENT_PATH}/ca.crt
 kubectl config set-credentials default-admin --token=`cat token`
 kubectl config set-context default-system --cluster=default-cluster --user=default-admin --namespace ${KUBE_NAMESPACE}
 kubectl config use-context default-system
